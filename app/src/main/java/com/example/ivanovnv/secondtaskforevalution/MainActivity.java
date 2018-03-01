f0c680775da75bbb10f547e427f28cfa18c60f84
alien1900 <alien1900@bk.ru>
2018-03-01 10:29:29 +0700

    stage1

--- app/src/main/java/com/example/ivanovnv/secondtaskforevalution/MainActivity.java
+++ app/src/main/java/com/example/ivanovnv/secondtaskforevalution/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ivanovnv.secondtaskforevalution;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    Create main menu with three items: settings, search and exit;
+    */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /*
+    When select menu item program show toast with menu item name
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
